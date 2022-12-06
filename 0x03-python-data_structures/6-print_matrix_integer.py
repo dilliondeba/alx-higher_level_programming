9e7317e7456f738342ceddf33179feecf1ae1a92
Dillion Dalu <daluudeze@gmail.com>
2022-12-06 09:33:59 -0800

    task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in range(len(matrix)):  # iteration for rows
+        for j in range(len(matrix[i])):  # iteration for the items in the row
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):  # $ will have no space
+                print(" ", end="")
+
+    print("")
