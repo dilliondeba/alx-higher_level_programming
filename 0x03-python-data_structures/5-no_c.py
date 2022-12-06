bd57f51938c22825e0c27b94ffd8bce9110795a0
Dillion Dalu <daluudeze@gmail.com>
2022-12-06 09:07:48 -0800

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
+def new_in_list(my_list, idx, element):
+    items = len(my_list) - 1
 
-def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    if idx < 0 or idx > items:
+        return my_list
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
