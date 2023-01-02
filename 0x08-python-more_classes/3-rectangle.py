8176c9202d3c359e3c2b89911392c74d63fdc800
Dillion Dalu <daluudeze@gmail.com>
2023-01-02 15:40:27 -0800

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -2,6 +2,8 @@
 """
 Defines a class Rectangle
 """
+
+
 class Rectangle:
     """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
@@ -24,7 +26,6 @@ class Rectangle:
         self.__width = value
 
     @property
-
     def height(self):
         """getter for the private instance attribute height"""
         return self.__height
